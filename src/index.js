6eab76cfe4c04a2f5cbc450db5d4d8903e432bee
Oswaldo <oswaldo.aj29@outlook.com>
2021-04-12 16:15:59 -0500

    CSS custom

--- src/index.js
+++ src/index.js
@@ -54,7 +54,6 @@ app.use((req, res, next) =>{
 
 //Routes
 app.use(require("./routes/index"));
-//app.use(require("./routes/notes"));
 app.use(require("./routes/users"));
 app.use(require("./routes/admin"));
 
