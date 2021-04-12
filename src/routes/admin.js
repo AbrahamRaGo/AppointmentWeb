761446908574f1041fe679c762f58b42cfb2ca62
Abraham <abrahamrg.2412@gmail.com>
2021-04-12 16:27:43 -0500

    Insertando y mostrando registros correctamente

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -8,8 +8,9 @@ router.get('/admin/createService', (req, res) =>{
     res.render('admin/createService', {layout: 'admin'});
 });
 
-router.get('/admin/services', (req, res) =>{
-    res.render('admin/services', {layout: 'admin'});
+router.get('/admin/services', async (req, res) =>{
+    const services = await Service.find()
+    res.render('admin/services', {services, layout: 'admin'});
 });
 
 router.get('/admin/calendar', (req, res) =>{
