8a3e5cb1a3eb45eba13c3469ed9ce04c4d8ef4da
Oswaldo <oswaldo.aj29@outlook.com>
2021-04-12 14:37:54 -0500

    Layouts and navigation

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,15 +3,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/admin/createService', (req, res) =>{
-    res.render('admin/createService');
+    res.render('admin/createService', {layout: 'admin'});
 });
 
 router.get('/admin/services', (req, res) =>{
-    res.render('admin/services');
+    res.render('admin/services', {layout: 'admin'});
 });
 
 router.get('/admin/calendar', (req, res) =>{
-    res.render('admin/calendar');
+    res.render('admin/calendar', {layout: 'admin'});
 });
 
 router.post('/admin/createService', (req, res) =>{
