6784b944e9bc02db382f6d1527df4d746856165e
Abraham <abrahamrg.2412@gmail.com>
2021-04-12 23:07:29 -0500

    Agendas

--- src/routes/client.js
+++ src/routes/client.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/client/agenda', (req, res)=>{
-    res.render('client/agenda', {layout: 'client'});
+const Service = require("../models/Service");
+const Appointment = require("../models/Appointment");
+
+router.get('/client/agenda', async (req, res)=>{
+    const services = await Service.find()
+    res.render('client/agenda', {services, layout: 'client'});
 }); //ruta para visitar la pagina inicial me devuelve el mensaje "index" o especificado cuando es send si es render envia a un archivo.
 
 module.exports = router;
