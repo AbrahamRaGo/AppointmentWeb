e8a7ff9ad413866e79a8b4da99b7deab1708338c
Abraham <abrahamrg.2412@gmail.com>
2021-04-13 00:38:02 -0500

    Haciendo citas

--- src/models/Appointment.js
+++ src/models/Appointment.js
@@ -5,7 +5,8 @@ const AppointmentSchema = new Schema({
     name: { type: String, require: true },
     mail: { type: String, require: true },
     service: { type: String, require: true },
-    date: { type: Date, require: true }
+    date: { type: Date, require: true },
+    time: {type: String, require: true}
 });
 
 module.exports = mongoose.model('Appointment', AppointmentSchema);
--- src/routes/client.js
+++ src/routes/client.js
@@ -4,9 +4,40 @@ const router = express.Router();
 const Service = require("../models/Service");
 const Appointment = require("../models/Appointment");
 
-router.get('/client/agenda', async (req, res)=>{
+router.get('/client/createAppointment', async (req, res)=>{
     const services = await Service.find()
-    res.render('client/agenda', {services, layout: 'client'});
+    const appointments = await Appointment.find()
+    res.render('client/createAppointment', {services, appointments, layout: 'client'});
 }); //ruta para visitar la pagina inicial me devuelve el mensaje "index" o especificado cuando es send si es render envia a un archivo.
+
+router.post('/client/createAppointment', async (req, res) => {
+    console.log(req.body)
+    const {nameClient, mailClient, date, time, service} = req.body;
+    const errors = []
+    if (!nameClient) {
+        errors.push({text: "Debe ingresar su nombre"})
+    }
+    if (!mailClient) {
+        errors.push({text: "Debe ingresar su correo"})
+    }
+    if (!date) {
+        errors.push({text: "Debe ingresar una fecha"})
+    }
+    if (!time != "Seleccionar") {
+        errors.push({text: "Debe ingresar una hora"})
+    }
+    if (service != "Seleccionar") {
+        errors.push({text: "Debe seleccionar un servicio"})
+    }
+    if (errors.length > 0) {
+        res.render("client/createAppointment", {errors, layout: "main"})
+    }
+    else{
+        const newAppointment = new Appointment({ nameClient, mailClient, service, date, time });
+        await newAppointment.save();
+        req.flash("success_msg", "Cita creada correctamente");
+        res.redirect("/client/createAppointment");
+    }
+})
 
 module.exports = router;
