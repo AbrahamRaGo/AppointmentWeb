db554405c3fe0f3c3caffc3cbf63c0fb473d05e3
Oswaldo <oswaldo.aj29@outlook.com>
2021-04-12 20:23:16 -0500

    Agenda en cliente

--- src/routes/client.js
+++ src/routes/client.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/client/agenda', (req, res)=>{
-    res.render('client/agenda');
+    res.render('client/agenda', {layout: 'client'});
 }); //ruta para visitar la pagina inicial me devuelve el mensaje "index" o especificado cuando es send si es render envia a un archivo.
 
 module.exports = router;
