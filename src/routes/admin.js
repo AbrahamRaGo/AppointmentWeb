a6be3e5c8e840d9ad8efa42d6c318ec5e6b5789d
Abraham <abrahamrg.2412@gmail.com>
2021-04-12 19:40:25 -0500

    Validaciones y mensajes de correcto, vista de citas ya es dinámica

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,28 +1,63 @@
-
 const express = require("express");
 const router = express.Router();
 
-const Service = require('../models/Service')
+const Service = require("../models/Service");
+const Appointment = require("../models/Appointment");
 
-router.get('/admin/createService', (req, res) =>{
-    res.render('admin/createService', {layout: 'admin'});
+// Rutas para servicios (Mostrar, editar y eliminar desde BD)
+router.get("/admin/createService", (req, res) => {
+  res.render("admin/createService", { layout: "admin" });
 });
 
-router.get('/admin/services', async (req, res) =>{
-    const services = await Service.find()
-    res.render('admin/services', {services, layout: 'admin'});
+router.get("/admin/services", async (req, res) => {
+  const services = await Service.find();
+  res.render("admin/services", { services, layout: "admin" });
 });
 
-router.get('/admin/calendar', (req, res) =>{
-    res.render('admin/calendar', {layout: 'admin'});
+router.post("/admin/createService", async (req, res) => {
+  const { service, cost, description } = req.body;
+  if (!service || !cost) {
+    req.flash("error_msg", "Los campos nombre y costo no deben estar vacíos");
+    res.redirect("/admin/createService");
+  } else {
+    const newService = new Service({ service, cost, description });
+    await newService.save();
+    req.flash("success_msg", "Servicio agregado correctamente");
+    res.redirect("/admin/services");
+  }
 });
 
-router.post('/admin/createService', async (req, res) =>{
-    const {service, cost, description} = req.body;
+router.get("/admin/update/:id", async (req, res) => {
+  const service = await Service.findById(req.params.id);
+  res.render("admin/updateService", { service, layout: "admin" });
+});
 
-        const newService = new Service({ service, cost, description });
-        await newService.save();
-        res.redirect('/admin/services')
-    
-})
+router.put("/admin/updateService/:id", async (req, res) => {
+  const { service, cost, description } = req.body;
+  if (!service || !cost) {
+    req.flash("error_msg", "Los campos nombre y costo no deben estar vacíos");
+    res.redirect(`/admin/update/${req.params.id}`);
+  } else {
+    await Service.findByIdAndUpdate(req.params.id, {
+      service,
+      cost,
+      description,
+    });
+    req.flash("success_msg", "Servicio actualizado correctamente");
+    res.redirect("/admin/services");
+  }
+});
+
+router.get("/admin/delete/:id", async (req, res) => {
+  await Service.findByIdAndDelete(req.params.id);
+  req.flash("success_msg", "Servicio eliminado correctamente");
+  res.redirect("/admin/services");
+});
+
+// Ruta para citas, (Unicamente mostrar)
+router.get("/admin/calendar", async (req, res) => {
+  const appointments = await Appointment.find();
+  res.render("admin/calendar", { appointments, layout: "admin" });
+});
+
 module.exports = router;
